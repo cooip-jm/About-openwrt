957d16bb6913982fdbc596b5d1e4975192e14621
cooip-jm <78735965+cooip-jm@users.noreply.github.com>
2023-09-30 17:26:11 +0800

    Create install-passwall.sh

---
+++ install-passwall.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+###
+echo "仅限Arm系统"
+
+echo "换源"
+sed -i 's/downloads.openwrt.org/mirrors.tuna.tsinghua.edu.cn\/openwrt/g' /etc/opkg/distfeeds.conf
+#
+echo "下载"
+wget https://github.com/xiaorouji/openwrt-passwall/releases/download/4.70-10/passwall_packages_ipk_aarch64_generic.zip -O passwall_packages_ipk_aarch6
+4_generic.zip
+
+wget https://github.com/xiaorouji/openwrt-passwall/releases/download/4.70-10/luci-app-passwall_4.70-10_all.ipk -O luci-app-passwall_4.70-10_all.ipk
+Downloading 'https://github.com/xiaorouji/openwrt-passwall/releases/download/4.70-10/luci-app-passwall_4.70-10_all.ipk'
+
+wget https://github.com/xiaorouji/openwrt-passwall/releases/download/4.70-10/luci-i18n-passwall-zh-cn_git-23.263.27645-0fab584_all.ipk -O luci-i18n-pa
+sswall-zh-cn_git-23.263.27645-0fab584_all.ipk
+
+echo "解压缩"
+unzip passwall_packages_ipk_aarch64_generic.zip
+
+echo "安装"
+
+opkg install *.ipk
+
+#
+echo "结束，准备重启openwrt"
+reboot
