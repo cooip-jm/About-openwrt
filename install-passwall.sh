c36f5141aff895ad59de4936b59174f80cd71531
cooip-jm <78735965+cooip-jm@users.noreply.github.com>
2023-09-30 17:42:48 +0800

    Update install-passwall.sh

--- install-passwall.sh
+++ install-passwall.sh
@@ -8,14 +8,12 @@ sed -i 's/downloads.openwrt.org/mirrors.tuna.tsinghua.edu.cn\/openwrt/g' /etc/op
 opkg update
 #
 echo "下载"
-wget https://github.com/xiaorouji/openwrt-passwall/releases/download/4.70-10/passwall_packages_ipk_aarch64_generic.zip -O passwall_packages_ipk_aarch6
-4_generic.zip
+wget https://github.com/xiaorouji/openwrt-passwall/releases/download/4.70-10/passwall_packages_ipk_aarch64_generic.zip -O passwall_packages_ipk_aarch64_generic.zip
 
 wget https://github.com/xiaorouji/openwrt-passwall/releases/download/4.70-10/luci-app-passwall_4.70-10_all.ipk -O luci-app-passwall_4.70-10_all.ipk
-Downloading 'https://github.com/xiaorouji/openwrt-passwall/releases/download/4.70-10/luci-app-passwall_4.70-10_all.ipk'
 
-wget https://github.com/xiaorouji/openwrt-passwall/releases/download/4.70-10/luci-i18n-passwall-zh-cn_git-23.263.27645-0fab584_all.ipk -O luci-i18n-pa
-sswall-zh-cn_git-23.263.27645-0fab584_all.ipk
+
+wget https://github.com/xiaorouji/openwrt-passwall/releases/download/4.70-10/luci-i18n-passwall-zh-cn_git-23.263.27645-0fab584_all.ipk -O luci-i18n-passwall-zh-cn_git-23.263.27645-0fab584_all.ipk
 
 echo "解压缩"
 unzip passwall_packages_ipk_aarch64_generic.zip
