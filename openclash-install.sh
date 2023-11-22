9da96545e8b43ef4b3cffed9f1fa7a5c6e27fac6
cooip-jm <78735965+cooip-jm@users.noreply.github.com>
2023-11-22 13:14:27 +0800

    Update openclash-install.sh

--- openclash-install.sh
+++ openclash-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #官方openwrt安装Openclash、中文、和设置时区脚本。
 #或者打自己单行复制粘贴执行。
+### wget -O - https://raw.githubusercontent.com/cooip-jm/About-openwrt/main/openclash-install.sh | sh -s -- -v
 #不限制系统架构，Openclash 版本更新自行替换脚本内下载地址
 echo "换源"
 sed -i 's/downloads.openwrt.org/mirrors.tuna.tsinghua.edu.cn\/openwrt/g' /etc/opkg/distfeeds.conf
@@ -12,7 +13,7 @@ opkg update
 opkg install luci-i18n-base-zh-cn coreutils-nohup bash dnsmasq-full curl ca-certificates ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag unzip kmod-nft-tproxy luci-compat luci luci-base
 #
 echo "下载openclash141"
-wget https://github.com/vernesong/OpenClash/releases/download/v0.45.141-beta/luci-app-openclash_0.45.141-beta_all.ipk -O luci-app-openclash_0.45.141-beta_all.ipk
+wget https://github.com/vernesong/OpenClash/releases/download/v0.45.152-beta/luci-app-openclash_0.45.152-beta_all.ipk -O luci-app-openclash_0.45.141-beta_all.ipk
 
 opkg install luci-app-openclash_0.45.141-beta_all.ipk
 
